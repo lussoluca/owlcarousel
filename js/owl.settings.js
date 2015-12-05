7bd687c0a9190b1570d569a0044100ce7adcf14a
ebutera <ebutera@wellnet.it>
2015-12-05 15:08:39 +0100

    owlCarousel - twig & js init

--- js/owl.settings.js
+++ js/owl.settings.js
@@ -1,3 +1,31 @@
-(function($, Drupal){
+/**
+ * @file
+ * Initiate Owl Carousel.
+ */
 
-})(jQuery, Drupal);
+(function ($) {
+
+    Drupal.behaviors.owlCarousel = {
+        attach: function (context, settings) {
+
+            for (var carousel in settings.owlcarousel) {
+                // Carousel instance.
+                var owl = $('.js-owlCarousel--' + carousel);
+
+                // lazyLoad support.
+                if (settings.owlcarousel[carousel].settings.lazyLoad) {
+                    var images = owl.find('img');
+
+                    $.each(images, function(i, image) {
+                        $(image).attr('data-src', $(image).attr('src'));
+                    });
+
+                    images.addClass('owl-lazy');
+                }
+
+                // Attach instance settings.
+                owl.owlCarousel(settings.owlcarousel[carousel].settings);
+            }
+        }
+    }
+})(jQuery);
